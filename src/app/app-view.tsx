75a17f75f6f46bce557eaa67e0516f0803dc9821
Stepan <stepan.lagunovsky@gmail.com>
2020-05-29 18:01:09 +0300

    Move scrollbars in to ui, remove scrolllbars from app-view

--- src/app/app-view.tsx
+++ src/app/app-view.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import { RouterProvider } from 'react-router5';
-import Scrollbars from 'react-custom-scrollbars';
 
 import Providers from './providers/providers';
 import { Layout } from './layout';
 
-// App providers
 export const AppView = ({ router }: any) => (
   <RouterProvider router={router}>
     <Providers>
-      <Scrollbars
-        autoHeight
-        autoHeightMin="100vh"
-        autoHide
-        autoHideTimeout={500}
-        autoHideDuration={200}
-      >
-        <Layout />
-      </Scrollbars>
+      <Layout />
     </Providers>
   </RouterProvider>
 );
