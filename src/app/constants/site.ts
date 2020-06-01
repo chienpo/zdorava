2405c0b6fa790d1e1440e248e8c52d542873f66b
Stepan <stepan.lagunovsky@gmail.com>
2020-06-01 15:33:56 +0300

    Update path in to https for portfolio items

--- src/app/constants/site.ts
+++ src/app/constants/site.ts
@@ -1,3 +1,3 @@
 export const SITE_PUBLICATION_YEAR = '2017-20';
-export const SITE_URL = 'http://zdorava.com/';
+export const SITE_URL = 'https://zdorava.com/';
 export const PORTFOLIO_IMAGES_PATH = 'assets/templates/html/images/portfolio/';
