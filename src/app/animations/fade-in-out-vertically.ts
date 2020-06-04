ad591b8ea9c4ccf94edc597928aad82c4267ef0b
Stepan <stepan.lagunovsky@gmail.com>
2020-06-04 12:51:20 +0300

    Remove unused mobile navigation view, and unused animation

--- src/app/animations/fade-in-out-vertically.ts
+++ src/app/animations/fade-in-out-vertically.ts
@@ -1,24 +1,5 @@
 import posed from 'react-pose';
 
-export const FadeInOut = posed.div({
-  enter: {
-    transition: {
-      default: { duration: 400 },
-      delay: 400,
-    },
-    opacity: 1,
-    zIndex: 5,
-  },
-  exit: {
-    opacity: 0,
-    zIndex: 5,
-    transition: {
-      duration: 200,
-      delay: 400,
-    },
-  },
-});
-
 export const FadeInOutVertically = posed.div({
   enter: {
     transition: {
