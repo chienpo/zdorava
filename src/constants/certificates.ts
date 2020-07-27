998172b563c6cad6c71701db19ab42707af6c170
Stepan <stepan.lagunovsky@gmail.com>
2020-07-27 13:58:01 +0300

    Update about description, add sertificates, add translations

--- src/constants/certificates.ts
+++ src/constants/certificates.ts
@@ -1,38 +1,61 @@
-import reactUdemyCourse from '../assets/images/sertificates/react-udemy-course.jpg';
-import typescriptUdemyCourse from '../assets/images/sertificates/type-script-udemy-course.jpg';
-import spokenIntermediateOneCourseCertificate from '../assets/images/sertificates/spoken-intermediate-one-course-lagunovsky.png';
-import businessEnglishThreeCourseCertificate from '../assets/images/sertificates/business-english-three-course-lagunovsky.png';
+import { i18nMark } from '@lingui/react';
+
+import reactUdemyCourse from 'assets/images/sertificates/react-udemy-course.jpg';
+import javaScriptUdemyCourse from 'assets/images/sertificates/java-script-udemy-course.jpg';
+import typeScriptUdemyCourse from 'assets/images/sertificates/type-script-udemy-course.jpg';
+import spokenIntermediateOneCourseCertificate from 'assets/images/sertificates/spoken-intermediate-one-course-lagunovsky.png';
+import businessEnglishThreeCourseCertificate from 'assets/images/sertificates/business-english-three-course-lagunovsky.png';
 
 interface Certificate {
   src: string;
   alt: string;
-  itemName: string;
-  itemDescription: string;
+  title: string;
+  description: string;
 }
 
 export const CERTIFICATES_DATA: Certificate[] = [
   {
     src: reactUdemyCourse,
-    alt: 'udemy react course certificate',
-    itemName: 'asd',
-    itemDescription: 'asd',
+    alt: i18nMark('react course certificate'),
+    title: i18nMark(
+      '"React - The Complete Guide (incl Hooks, React Router, Redux)"'
+    ),
+    description: i18nMark(
+      '"React - The Complete Guide (incl Hooks, React Router, Redux)" certificate, "UDEMY"'
+    ),
   },
   {
-    src: typescriptUdemyCourse,
-    alt: 'udemy type script course certificate',
-    itemName: 'asd',
-    itemDescription: 'asd',
+    src: javaScriptUdemyCourse,
+    alt: i18nMark('java-script course certificate'),
+    title: i18nMark(
+      '"JavaScript - The Complete Guide 2020 (Beginner + Advanced)"'
+    ),
+    description: i18nMark(
+      '"JavaScript - The Complete Guide 2020 certificate, "UDEMY"'
+    ),
+  },
+  {
+    src: typeScriptUdemyCourse,
+    alt: i18nMark('type-script course certificate'),
+    title: i18nMark('"Understanding TypeScript - 2020 Edition"'),
+    description: i18nMark(
+      '"Understanding TypeScript - 2020 Edition" certificate, "UDEMY"'
+    ),
   },
   {
     src: spokenIntermediateOneCourseCertificate,
-    alt: 'spoken intermediate one course certificate lagunovsky',
-    itemName: 'asd',
-    itemDescription: 'asd',
+    alt: i18nMark('spoken intermediate one english course certificate'),
+    title: i18nMark('"Spoken intermediate english course"'),
+    description: i18nMark(
+      '"Spoken intermediate one course" certificate, Itransition'
+    ),
   },
   {
     src: businessEnglishThreeCourseCertificate,
-    alt: 'business english three course certificate lagunovsky',
-    itemName: 'asd',
-    itemDescription: 'asd',
+    alt: i18nMark('business english three english course certificate'),
+    title: i18nMark('"Business english three english course"'),
+    description: i18nMark(
+      '"Business english three course" certificate, Itransition'
+    ),
   },
 ];
--- src/features/about/components/certifiates/certificates.tsx
+++ src/features/about/components/certifiates/certificates.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Lightbox from 'react-image-lightbox';
+import { I18n } from '@lingui/react';
 import 'react-image-lightbox/style.css';
 
+import { RED } from 'constants/colors';
 import { CERTIFICATES_DATA } from 'constants/certificates';
 import { Figure, Img, GalleryRow } from './styled';
 
@@ -17,47 +19,51 @@ export const Certificates = () => {
   };
 
   return (
-    <GalleryRow>
-      {isOpen && (
-        <Lightbox
-          mainSrc={CERTIFICATES_DATA[photoIndex].src}
-          nextSrc={
-            CERTIFICATES_DATA[(photoIndex + 1) % CERTIFICATES_DATA.length].src
-          }
-          prevSrc={
-            CERTIFICATES_DATA[
-              (photoIndex + data.length - 1) % CERTIFICATES_DATA.length
-            ].src
-          }
-          onCloseRequest={() => setIsOpen(false)}
-          onMovePrevRequest={() =>
-            setPhotoIndex(
-              (photoIndex + data.length - 1) % CERTIFICATES_DATA.length
-            )
-          }
-          onMoveNextRequest={() =>
-            setPhotoIndex((photoIndex + 1) % CERTIFICATES_DATA.length)
-          }
-          imageTitle={
-            CERTIFICATES_DATA[(photoIndex + 1) % CERTIFICATES_DATA.length]
-              .itemName
-          }
-          imageCaption={
-            CERTIFICATES_DATA[(photoIndex + 1) % CERTIFICATES_DATA.length]
-              .itemDescription
-          }
-          reactModalStyle={{ color: 'yellow' }}
-          imagePadding={100}
-          wrapperClassName="wrapperClassName"
-          prevLabel="previous-project"
-          nextLabel="next-project"
-        />
+    <I18n>
+      {({ i18n }) => (
+        <GalleryRow>
+          {isOpen && (
+            <Lightbox
+              mainSrc={CERTIFICATES_DATA[photoIndex].src}
+              nextSrc={
+                CERTIFICATES_DATA[(photoIndex + 1) % CERTIFICATES_DATA.length]
+                  .src
+              }
+              prevSrc={
+                CERTIFICATES_DATA[
+                  (photoIndex + data.length - 1) % CERTIFICATES_DATA.length
+                ].src
+              }
+              onCloseRequest={() => setIsOpen(false)}
+              onMovePrevRequest={() =>
+                setPhotoIndex(
+                  (photoIndex + data.length - 1) % CERTIFICATES_DATA.length
+                )
+              }
+              onMoveNextRequest={() =>
+                setPhotoIndex((photoIndex + 1) % CERTIFICATES_DATA.length)
+              }
+              imageTitle={i18n._(
+                CERTIFICATES_DATA[photoIndex % CERTIFICATES_DATA.length].title
+              )}
+              imageCaption={i18n._(
+                CERTIFICATES_DATA[photoIndex % CERTIFICATES_DATA.length]
+                  .description
+              )}
+              reactModalStyle={{ color: RED }}
+              imagePadding={100}
+              wrapperClassName="wrapperClassName"
+              prevLabel="previous-project"
+              nextLabel="next-project"
+            />
+          )}
+          {CERTIFICATES_DATA.map(({ src, alt }, index) => (
+            <Figure key={src} onClick={() => onItemClick(index)}>
+              <Img src={src} alt={alt} />
+            </Figure>
+          ))}
+        </GalleryRow>
       )}
-      {CERTIFICATES_DATA.map(({ src, alt }, index) => (
-        <Figure key={src} onClick={() => onItemClick(index)}>
-          <Img src={src} alt={alt} />
-        </Figure>
-      ))}
-    </GalleryRow>
+    </I18n>
   );
 };
