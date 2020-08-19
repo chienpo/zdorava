60ea6b1559bba266d44301b733cb144ea8830e07
Stepan <stepan.lagunovsky@gmail.com>
2020-08-19 20:12:27 +0300

    Update snapshot for themeswitch

--- src/ui/theme-switch/theme-switch.spec.tsx
+++ src/ui/theme-switch/theme-switch.spec.tsx
@@ -3,8 +3,16 @@ import { shallow } from 'enzyme';
 
 import { ThemeSwitchView as ThemeSwitch } from './theme-switch-view';
 
-it('should render ThemeSwitch component ', () => {
-  const wrapper = shallow(<ThemeSwitch />);
+describe('ThemeSwitch component', () => {
+  it('should render ThemeSwitch component with props', () => {
+    const wrapper = shallow(
+      <ThemeSwitch onChange={val => val} checked disabled />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 
-  expect(wrapper).toMatchSnapshot();
+  it('should render ThemeSwitch component without props', () => {
+    const wrapper = shallow(<ThemeSwitch />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
