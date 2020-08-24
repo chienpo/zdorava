bbe1ea67c2efdf7cdb82ca68168e149214fac889
Stepan <stepan.lagunovsky@gmail.com>
2020-08-24 17:06:45 +0300

    Update stylelint config, separate rules with comment

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,8 +2,9 @@ module.exports = {
   processors: ['stylelint-processor-styled-components'],
   extends: [
     'stylelint-config-standard',
-    // "stylelint-config-standard",
+    // "stylelint-config-rational-order",
     'stylelint-config-rational-order',
+    // ...
     'stylelint-config-styled-components',
     'stylelint-config-prettier',
   ],
