79c5c8c7248127745411ffddfee686faec067b50
Stepan <stepan.lagunovsky@gmail.com>
2021-01-20 09:39:02 +0300

    Remove unused constant from api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,3 @@
-// export const BACKEND_URL = 'http://localhost:3000/';
 export const FIREBASE_AUTH_DOMAIN = 'zdorava-9a8e1.firebaseio.com';
 export const FIREBASE_DATABASE_URL = 'https://zdorava-9a8e1.firebaseio.com';
 export const FIREBASE_DATABASE_REF = 'portfolio';
