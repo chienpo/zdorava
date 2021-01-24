96d251a0892337954f402d4d35e99ab777b9b64e
Stepan <stepan.lagunovsky@gmail.com>
2021-01-24 20:41:13 +0300

    Update routes with canActivate method and add project-add route to forbidden menu routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,4 @@
-import { Route } from 'router5';
+import { Route, Router } from 'router5';
 
 import { STORAGE_ACTIVE_PORTFOLIO_CATEGORY_KEY } from '~/store/constants';
 
@@ -6,10 +6,25 @@ export const ROUTE_NAME_HOME = 'home';
 export const ROUTE_NAME_ABOUT = 'about';
 export const ROUTE_NAME_PORTFOLIO = 'portfolio';
 export const ROUTE_NAME_PROJECTS_ADD = 'add-project';
-export const ROUTE_NAME_PORTFOLIO_CATEGORY = 'category';
 export const ROUTE_NAME_PROJECT = 'project';
 export const ROUTE_NAME_PROJECT_EDIT = 'edit';
 export const ROUTE_NAME_SIGH_IN = 'sign-in';
+
+const canActivate = (dispatch: Router) => () => {
+  const selectedCategory = Boolean(
+    localStorage.getItem(STORAGE_ACTIVE_PORTFOLIO_CATEGORY_KEY)
+  );
+
+  if (!localStorage.getItem('userId')) {
+    dispatch.navigate(
+      'portfolio',
+      { category: selectedCategory },
+      { reload: true }
+    );
+    return false;
+  }
+  return true;
+};
 
 export const routes: Route[] = [
   { name: ROUTE_NAME_HOME, path: '/' },
@@ -19,34 +34,26 @@ export const routes: Route[] = [
   {
     name: ROUTE_NAME_PROJECT,
     path: '/portfolio/:id',
-    children: [{ name: ROUTE_NAME_PROJECT_EDIT, path: '/edit' }],
+    children: [
+      {
+        name: ROUTE_NAME_PROJECT_EDIT,
+        path: '/edit',
+        canActivate,
+      },
+    ],
   },
   {
     name: ROUTE_NAME_PROJECTS_ADD,
     path: '/portfolio/add-project',
-    canActivate: (dispatch) => () => {
-      const selectedCategory = Boolean(
-        localStorage.getItem(STORAGE_ACTIVE_PORTFOLIO_CATEGORY_KEY)
-      );
-
-      if (!localStorage.getItem('userId')) {
-        dispatch.navigate(
-          'portfolio',
-          { category: selectedCategory },
-          { reload: true }
-        );
-        return false;
-      }
-      return true;
-    },
+    canActivate,
   },
 ];
 
 const forbiddenMenuRoutes = new Set([
-  ROUTE_NAME_PORTFOLIO_CATEGORY,
   ROUTE_NAME_PROJECT,
   ROUTE_NAME_PROJECT_EDIT,
   ROUTE_NAME_SIGH_IN,
+  ROUTE_NAME_PROJECTS_ADD,
 ]);
 
 export const menuRoutes = routes.filter(
