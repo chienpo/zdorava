708edb1256bfb6359d2bd99d09df8e8404febd6d
Stepan <stepan.lagunovsky@gmail.com>
2021-01-24 21:11:09 +0300

    Update AppBox and GridContent widths to remove horizontal scroll on zoom

--- src/core/styled.ts
+++ src/core/styled.ts
@@ -4,7 +4,6 @@ import { WHITE } from '~/constants/colors';
 import { AnimatedDiv } from '~/animations/animated';
 
 export const AppBox = styled.div`
-  width: 100vw;
   min-height: 100vh;
   background: ${WHITE};
 `;
@@ -13,7 +12,6 @@ export const MotionContent = styled(AnimatedDiv)`
   display: grid;
   grid-template-rows: auto 1fr auto;
   grid-template-columns: minmax(320px, 1fr);
-  width: 100vw;
   min-height: 100vh;
   overflow: hidden;
   background: ${WHITE};
